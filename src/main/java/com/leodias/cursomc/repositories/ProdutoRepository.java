b4530adb0305689a116d90181897bb68e4f30ae4
Leo Dias <leodiascarvalhosr@gmail.com>
2022-07-31 16:03:09 -0300

    Produto e associação muito para muitos

---
+++ src/main/java/com/leodias/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.leodias.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leodias.cursomc.dominios.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
