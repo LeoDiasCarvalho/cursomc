176f2127f32bbb583348a99d17c3f7fe79ec01e9
Leo Dias <leodiascarvalhosr@gmail.com>
2022-08-04 21:53:20 -0300

    Endpoint/clientes/{id}

---
+++ src/main/java/com/leodias/cursomc/resources/ClienteResource.java
@@ -0,0 +1,29 @@
+package com.leodias.cursomc.resources;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leodias.cursomc.dominios.Cliente;
+import com.leodias.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> buscarPorId(@PathVariable Integer id) {
+		
+		Optional<Cliente> obj = service.buscarPorId(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/leodias/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.leodias.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leodias.cursomc.dominios.Cliente;
+import com.leodias.cursomc.repositories.ClienteRepository;
+import com.leodias.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Optional<Cliente> buscarPorId(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		
+		return Optional.of(obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())));
+	}
+}
