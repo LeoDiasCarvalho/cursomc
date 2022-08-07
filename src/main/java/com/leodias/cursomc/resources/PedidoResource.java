d502ae1177df0c4b9473c800e09a0c0fb1e65b2a
Leo Dias <leodiascarvalhosr@gmail.com>
2022-08-07 08:36:39 -0300

    Endpoint/ pedidos/ {id} disponibilizado

---
+++ src/main/java/com/leodias/cursomc/resources/PedidoResource.java
@@ -0,0 +1,29 @@
+package com.leodias.cursomc.resources;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leodias.cursomc.dominios.Pedido;
+import com.leodias.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value = "/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService service;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> buscarPorId(@PathVariable Integer id) {
+		
+		Optional<Pedido> obj = service.buscarPorId(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/leodias/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.leodias.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leodias.cursomc.dominios.Pedido;
+import com.leodias.cursomc.repositories.PedidoRepository;
+import com.leodias.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Optional<Pedido> buscarPorId(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+
+		return Optional.of(obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName())));
+	}
+}
